e445979194977b27e6fde72f7ba337072cbe7b43
kPherox <kphrx@kpherox.dev>
2023-06-17 17:21:49 +0900

    feat: mocking console.log

--- src/index-test.js
+++ src/index-test.js
@@ -1,3 +1,16 @@
+import assert from 'node:assert';
+import { mock } from 'node:test';
+
 import main from './index.js';
 
+mock.method(console, 'log', () => {});
+
+assert.strictEqual(console.log.mock.callCount(), 0);
 main();
+assert.strictEqual(console.log.mock.callCount(), 1);
+
+const call = console.log.mock.calls[0];
+
+assert.deepStrictEqual(call.arguments, ['olive branch']);
+assert.strictEqual(call.result, undefined);
+assert.strictEqual(call.this, console);
