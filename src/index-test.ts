900d3b306beab56ced52fbf5e3f1d92ed3d319a4
kPherox <kphrx@kpherox.dev>
2023-06-17 18:20:18 +0900

    feat: with TypeScript

--- src/index-test.ts
+++ src/index-test.ts
@@ -3,13 +3,13 @@ import { mock } from 'node:test';
 
 import main from './index.js';
 
-mock.method(console, 'log', () => {});
+const log = mock.method(console, 'log', () => {});
 
-assert.strictEqual(console.log.mock.callCount(), 0);
+assert.strictEqual(log.mock.callCount(), 0);
 main();
-assert.strictEqual(console.log.mock.callCount(), 1);
+assert.strictEqual(log.mock.callCount(), 1);
 
-const call = console.log.mock.calls[0];
+const call = log.mock.calls[0];
 
 assert.deepStrictEqual(call.arguments, ['olive branch']);
 assert.strictEqual(call.result, undefined);
